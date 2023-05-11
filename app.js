3ec8fb446a0c81d52b2e0980f66f5bc0e96a87ac
Alexey Belyaev <lebelr@yandex.ru>
2023-05-11 19:56:44 +0500

    Добавление логгеров запросов и ошибок

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const handlerErrors = require('./middlewares/handlerErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -14,7 +15,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use('/', router);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(handlerErrors);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,8 @@ function auth(req, res, next) {
   try {
     payload = jwt.verify(token, 'secret');
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    res.status(401).send({ message: 'Необходима авторизация' });
+    return;
   }
 
   req.user = payload;
