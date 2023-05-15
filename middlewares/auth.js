bc8c185faad92baaa043da80ab46f552963f7dbe
Alexey Belyaev <lebelr@yandex.ru>
2023-05-15 20:08:52 +0500

    Исправление мидлвары авторизаии и добавление env файла

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,27 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 function auth(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    res.status(401).send({ message: 'Необходима авторизация' });
-    return;
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
 
-  next();
+  return next();
 }
 
 module.exports = auth;
